79af1ff7cce02937b95da61a8b1ec713069cae8c
Andrea Restrepo <andrearestrepox@gmail.com>
2022-09-06 23:35:01 -0400

    fixing circle animation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
-import liquid from '../assets/img/liquidswirl.jpg'
 import roomimg from '../assets/img/roomimg.jpg'
+import '../balls.css'
 
 
 function Skills() {
@@ -8,9 +8,27 @@ function Skills() {
         <div>
         
             <img className='liquid' src={roomimg} alt="liqud swirl image" />
-            <div className='rectangle2'>
+            <div>
             {/* <h2>Skills</h2> */}
              </div>
+           <div className='y'><div className='ball'><h3>Javascript</h3></div></div> 
+            <div className='ball1'><h3>CSS</h3></div>
+            <div className='ball2'><h3>HTML</h3></div>
+            <div className='ball3'></div>
+            <div className='ball4'></div>
+            <div className='ball5'></div>
+            <div className='ball6'></div>
+            <div className='ball7'></div>
+            <div className='ball8'></div>
+            <div className='ball9'></div>
+            {/* <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div> */}
+             
         </div>
         
     )
