8ed8e3589466e6c7ebd55575a35920629138951f
Andrea Restrepo <andrearestrepox@gmail.com>
2022-09-07 15:09:21 -0400

    editing skills content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,16 +11,16 @@ function Skills() {
             <div>
             {/* <h2>Skills</h2> */}
              </div>
-           <div className='y'><div className='ball'><h3>Javascript</h3></div></div> 
+           <div className='y'><h3>Javascript</h3></div> 
             <div className='ball1'><h3>CSS</h3></div>
             <div className='ball2'><h3>HTML</h3></div>
-            <div className='ball3'></div>
-            <div className='ball4'></div>
-            <div className='ball5'></div>
-            <div className='ball6'></div>
-            <div className='ball7'></div>
-            <div className='ball8'></div>
-            <div className='ball9'></div>
+            <div className='ball3'><h3>Postman</h3></div>
+            <div className='ball4'><h3>Express</h3></div>
+            <div className='ball5'><h3>React</h3></div>
+            <div className='ball6'><h3>MERN Stack</h3></div>
+            <div className='ball7'><h3>Visual Studio</h3></div>
+            <div className='ball8'><h3>Front-End Development</h3></div>
+            <div className='ball9'><h3>Back-End Development</h3></div>
             {/* <div className='ball'></div>
             <div className='ball'></div>
             <div className='ball'></div>
